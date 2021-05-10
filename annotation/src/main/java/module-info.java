ba3baf22768640eabad5430d2a9766d338edd578
ghubstan <36207203+ghubstan@users.noreply.github.com>
2021-05-10 19:37:27 -0300

    Add annotation module to control which are used

---
+++ annotation/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module org.misq.annotation {
+    requires transitive java.annotation;
+    requires transitive lombok;
+    exports org.misq.annotation;
+}
---
+++ annotation/src/main/java/org/misq/annotation/DummyAnnotation.java
@@ -0,0 +1,5 @@
+package org.misq.annotation;
+
+// Every module needs a closs.
+public class DummyAnnotation {
+}
